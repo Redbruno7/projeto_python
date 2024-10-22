9a17d43d939656bf25f5117ce3ba7aff9954ebee
Redbruno7 <brunorodgers7@gmail.com>
2024-10-22 16:07:03 -0300

    aula_01

---
+++ aula_01/hello_world.py
@@ -0,0 +1,18 @@
+# Curso Técnico de Desenvolvimento de Sistemas
+# Turma: 0392
+# Autor: Bruno C. Rodgers
+# Data: 22/10/2024
+
+# AULA 01 - PRIMEIRO PROGRAMA
+
+# Importar bibliotecas
+import os
+
+# Limpar terminal
+os.system('cls')
+
+# O comando print() executa uma saída via terminal
+print('-' * 70)
+print('Hello World!!!')
+print('-' * 70)
+print()
