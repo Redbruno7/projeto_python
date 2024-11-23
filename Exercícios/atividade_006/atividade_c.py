fb5d1b72d8e7280c01e8237fd818bfbd4ce54451
LauraFontana01 <fontanameiolaura@hotmail.com>
2024-11-23 14:16:02 -0300

    teste

--- Exercícios/atividade_006/atividade_c.py
+++ Exercícios/atividade_006/atividade_c.py
@@ -1,7 +1,14 @@
 # ATIVIDADE 006 - ATIVIDADE C
 # ALUNO: BRUNO C. RODGERS
 # Data: 22/11/2024
-# 
+# Faça um programa que procure na lista numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] e produza:
+# O intervalo de 1 até 9
+# O intervalo de 8 até 13
+# Os números pares
+# Os números ímpares
+# Os múltiplos de 2, 3 e 4
+# Lista reversa
+# O produto dos intervalos 5-6 por 11-12
 
 import os
 
