4d77fc8475432c17a669d60c370015aed1806706
LauraFontana01 <fontanameiolaura@hotmail.com>
2024-11-23 20:04:14 -0300

    d

--- Exercícios/atividade_006/atividade_d.py
+++ Exercícios/atividade_006/atividade_d.py
@@ -1,7 +1,7 @@
 # ATIVIDADE 006 - ATIVIDADE D
 # ALUNO: BRUNO C. RODGERS
 # Data: 22/11/2024
-# 
+# Faça um programa que preencha uma lista com as notas de 4 alunos, depois imprima a média.
 
 import os
 
@@ -9,9 +9,36 @@ import os
 os.system('cls')
 
 # Título
-print('=' * 70)
-print('')
-print('=' * 70)
+print('=' * 80)
+print('Média de Notas de Alunos')
+print('=' * 80)
 print()
 
 # Entrada
+lista_notas = []
+
+# Flag
+soma = 0
+
+# Iteração 4 alunos
+print('-' * 80)
+for i in range(1, 5):
+    nota = float(input(f'Digite a nota do {i}º aluno: '))
+    print('-' * 80)
+
+    # Inserção de valor na lista
+    lista_notas.append(f'{nota:.2f}')
+
+    # Soma
+    soma += nota
+print()
+
+# Calculo média
+media = soma / 4
+
+# Saída
+print('=' * 80)
+print(f'Notas dos alunos respectivamente inseridas: {lista_notas}')
+print('-' * 80)
+print(f'A média dos alunos é: {media:.2f}')
+print('=' * 80)
--- Exercícios/atividade_006/atividade_e.py
+++ Exercícios/atividade_006/atividade_e.py
@@ -1,7 +1,7 @@
 # ATIVIDADE 006 - ATIVIDADE E
 # ALUNO: BRUNO C. RODGERS
 # Data: 22/11/2024
-# 
+# Faça um programa que leia as vogais, depois mostre-as em ordem inversa.
 
 import os
 
@@ -10,8 +10,9 @@ os.system('cls')
 
 # Título
 print('=' * 70)
-print('')
+print('Leitura e Inversão de Vogais')
 print('=' * 70)
 print()
 
 # Entrada
+vogais = ['a', 'e', 'i', 'o', 'u']
