6ba7dabf97fc8a27492193f4e31d87d4a2c8a868
LauraFontana01 <fontanameiolaura@hotmail.com>
2024-11-23 16:24:02 -0300

    exercicio c

--- Exercícios/atividade_006/atividade_c.py
+++ Exercícios/atividade_006/atividade_c.py
@@ -17,8 +17,53 @@ os.system('cls')
 
 # Título
 print('=' * 70)
-print('')
+print('Fatiamento de Lista')
 print('=' * 70)
 print()
 
 # Entrada
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+multiplos = []
+
+# Fatiamento lista
+intervalo_1 = lista[0 : 9]
+intervalo_2 = lista[7 : 13]
+pares = lista[1 : 15 : 2]
+impares = lista[0 : 15 : 2]
+reversa = lista[::-1]
+intervalo_3 = lista[4 : 6]
+intervalo_4 = lista[10 : 12]
+
+# Iteração lista
+for i in lista:
+    
+    # Condição múltiplo de 2, 3 e 4
+    if (i % 2 == 0) or (i % 3 == 0) or (i % 4 == 0):
+        multiplos.append(i)
+
+# Produto dos intervalos 3 e 4
+produto = [
+    a * b
+    for a in intervalo_3
+    for b in intervalo_4
+]
+
+# Saída
+print('=' * 70)
+print(f'Lista inicial: {lista}')
+print('-' * 70)
+print(f'Intervalo de 1 a 9: {intervalo_1}')
+print('-' * 70)
+print(f'Intervalo de 8 a 13: {intervalo_2}')
+print('-' * 70)
+print(f'Números pares: {pares}')
+print('-' * 70)
+print(f'Números ímpares: {impares}')
+print('-' * 70)
+print(f'Múltiplos de 2, 3 e 4: {multiplos}')
+print('-' * 70)
+print(f'Lista revertida: {reversa}')
+print('-' * 70)
+print(f'Produto dos intervalos 5-6 por 11-12: {produto}')
+print('=' * 70)
+print()
