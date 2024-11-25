70253ac5b8fba57b521d5889ec96862c3a1f17f0
LauraFontana01 <fontanameiolaura@hotmail.com>
2024-11-25 12:46:01 -0300

    atividade007 - a

--- Exercícios/atividade_007/atividade_a.py
+++ Exercícios/atividade_007/atividade_a.py
@@ -17,6 +17,46 @@ os.system('cls')
 
 # Título
 print('=' * 70)
-print('')
+print('Leitura de Notas:')
+print('=' * 70)
+print()
+
+# Entrada
+lista_notas = []
+
+# Flag
+quantidade = 0
+
+# Iteração notas
+print('=' * 70)
+print('Insira as notas. Digite "s" ou "0" para sair.')
+print('-' * 70)
+while True:
+    nota = input('Digite uma nota: ').strip().lower()
+    print('-' * 70)
+
+    # Condição quebra de laço
+    if nota == 's' or nota == '0':
+        break
+    
+    # Inserção de valores na lista
+    lista_notas.append(float(nota))
+
+    # Contagem de notas inseridas
+    quantidade += 1
+print()
+
+# Lista inversa conforme inserção
+lista_inversa = lista_notas[::-1]
+
+# Saída
+print('=' * 70)
+print(f'Quantidade de notas inseridas na lista: {quantidade}')
+print('-' * 70)
+print(f'Lista de notas respectivamente inseridas: {lista_notas}')
+print('-' * 70)
+print(f'Lista de notas inversa conforme inserção: {lista_inversa}')
+print('-' * 70)
+
 print('=' * 70)
 print()
