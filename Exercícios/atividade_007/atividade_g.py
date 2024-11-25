51b4af0d9487650e2b01bf21480302e06e924ff2
Redbruno7 <brunorodgers7@gmail.com>
2024-11-25 15:55:12 -0300

    007 completo

--- Exercícios/atividade_007/atividade_g.py
+++ Exercícios/atividade_007/atividade_g.py
@@ -10,12 +10,23 @@ import random
 os.system('cls')
 
 # Título
-print('=' * 70)
+print('=' * 90)
 print('Simulador de sorteio da Mega Sena e Lotofácil:')
-print('=' * 70)
+print('=' * 90)
 print()
 
 # Entrada
 mega_sena = random.sample(range(1, 61), 6)
+lotofacil = random.sample(range(1, 26), 15)
+
+# Ordenação em lista
+mega_sena.sort()
+lotofacil.sort()
 
 # Saída
+print('=' * 90)
+print(f'Números sorteados Mega Sena: {mega_sena}')
+print('-' * 90)
+print(f'Números sorteados Lotofácil: {lotofacil}')
+print('=' * 90)
+print()
