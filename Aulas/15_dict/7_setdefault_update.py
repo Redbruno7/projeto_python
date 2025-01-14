a7334bf1790ae948bb17a8267e497592b1b40027
LauraFontana01 <fontanameiolaura@hotmail.com>
2025-01-14 10:57:39 -0300

    ...

---
+++ Aulas/15_dict/7_setdefault_update.py
@@ -0,0 +1,38 @@
+# AULA 15 - ESTRUTURA DE DADOS - DICIONÁRIO - MÉTODOS - SETDEFAULT / UPDATE
+# Data: 13/01/2024
+
+import os
+
+
+os.system('cls')
+
+# Título
+print('=' * 80)
+print('DICT - MÉTODOS - SETDEFAULT / UPDATE')
+print('-' * 80)
+print('Sintaxe 1: dicionário.pop(key, default)')
+print('-' * 80)
+print('Sintaxe 2: dicionário.keys()')
+print('=' * 80)
+print()
+
+# Dict
+my_dict = {}
+
+# Menu
+print('=' * 80)
+print('Menú de Opções:')
+print('-' * 80)
+print('1. Adicionar um par chave-valor.')
+print('-' * 80)
+print('2. Remover um item.')
+print('-' * 80)
+print('3. Remover o último item.')
+print('-' * 80)
+print('4. Mostrar dicionário atual.')
+print('-' * 80)
+print('5. Sair.')
+print('=' * 80)
+print()
+
+# Looping Menu
