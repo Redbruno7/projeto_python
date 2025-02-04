b2f8406a5a8d7acccfc6d986d5660f78e84fbd8f
LauraFontana01 <fontanameiolaura@hotmail.com>
2025-02-04 11:14:17 -0300

    Projeto B def modularizado completo!

--- Exercícios/atividade_010/project_b/main.py
+++ Exercícios/atividade_010/project_b/main.py
@@ -10,57 +10,23 @@ import os
 
 
 from modules.title import title
-from modules.get_datas import get_datas
+from modules.get_data import register_student
 
 # Limpar terminal
 def clear_terminal():
     os.system('cls')
 
-# Programa Principal
 clear_terminal()
 title()
 
-# Entrada
-print('=' * 80)
-while True:
-    name = input('Digite o nome do aluno: ').strip()
-    print('-' * 80)
-
-    if not name:
-        print('Nome inválido. Tente novamente.')
-        print('-' * 80)
-    else:
-        break
-
-while True:
-    register = input('Digite a matrícula do aluno: ')
-    print('-' * 80)
-
-    if not register.isdigit():
-        print('Matrícula inválida. Tente novamente.')
-        print('-' * 80)
-    else:
-        register = int(register)
-        break
-
-while True:
-    birth = input('Digite a data de nascimento do aluno (dd/mm/aaaa): ')
-
-    if not birth:
-        print('-' * 80)
-        print('Data de nascimento inválida. Tente novamente.')
-        print('-' * 80)
-    else:
-        break
-print('=' * 80)
-print()
+# Registro aluno
+student = register_student()
 
 # Saída
-data_dict = get_datas(nome = name, matricula = register, nascimento = birth)
 print('=' * 80)
 print('Informações do aluno:')
 print('-' * 80)
-for key, value in data_dict.items():
+for key, value in student.items():
     print(f'{key}: {value}')
 print('=' * 80)
 print()
